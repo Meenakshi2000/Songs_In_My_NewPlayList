10ee4a6d98afa0c103cdb321d1730192e115b2a3
Meenakshi2000 <44131450+Meenakshi2000@users.noreply.github.com>
2021-06-09 01:34:53 +0530

    My_Favourites

---
+++ song.js
@@ -0,0 +1,58 @@
+
+let songs = [ { Id: 0,
+    
+        name: "Ande ka fanda",
+        movie: "Jodi No. 1 " ,
+        path: "https://pagalfree.com/musics/128-Ande Ka Fanda - Jodi No.1.mp3"
+       
+        
+    },
+  { Id: 1,
+    name: "Jaane Kyon",
+        movie: "Dil Chahta Hai",
+        path: "https://pagalsong.in/uploads/systemuploads/mp3/Dil Chahta Hai/Jaane Kyon Log Pyar.mp3"
+    },
+  
+  { Id: 2,
+    name: "Bade Miyan",
+    movie: "Bade Miyan Chote Miyan",
+    path: "https://pagalsong.in/uploads/systemuploads/mp3/Bade Miyan Chote Miyan/Bade Miyan Toh Bade Miyan.mp3",
+},
+  { Id: 3,
+    name: "Pyar Ka Dushman",
+    movie: "Amar Akbar Anthony",
+    path: " https://jatt.download/mrisky/dren/data2/Hindi_Movies/201510/Amar_Akbar_Anthony/128/Taiyabali_Pyar_Ka_Dushman.mp3/Taiyabali Pyar Ka Dushman.mp3",
+},
+  { Id: 4,
+    name: "Main Jaadugar",
+        movie: "Jaadugar",
+        path: "https://jatt.download/dren/music/data/Hindi_Movies/201403/Jaadugar/48/Main_Jaadugar_3.mp3/Main Jaadugar 3.mp3",
+    },
+  { Id: 5,
+    name: "Paanch Rupaiya Baara Aana",
+    movie: "Chalti Ka Naam Gaadi",
+    path: "https://pagalworld.casa/download.php?id=9981.mp3",
+},
+  { Id: 6,
+    name: "Ek Chatur Naar",
+    movie: "Padosan",
+ 
+    path: "https://medium.cdnmp3.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGJ1bV9pZCI6NjksInRpbWVvdXQiOjE2MjMxNzE0NDV9.-ex2wFw82JXE0YM4QfwpC018aeQsa2i-x_zwrbzPGMg/bweeh/Ek%20Chatur%20Naar%20Karke%20Sringar%20-%20%28amlijatt.in%29.mp3",
+},
+  { Id: 7,
+    name: "Bombay Se Aaya Mera Dost",
+    movie: "Aap Ki Khatir",
+    path: "https://pagalworld.casa/download.php?id=11909",
+},
+  { Id: 8,
+    name: "Padosan Apni Murgi",
+        movie: "Jaadugar",
+        path: "  https://jatt.download/music/data/Hindi_Movies/201403/Jaadugar/128/Padosan_Apni_Murgi_Ko_Rakhna_5.mp3",
+    },
+    { Id: 9,
+        name: "Coolie No.1",
+        movie: "Coolie No.1",
+        path: "https://pagalfree.com/musics/128-Coolie No.1 - Coolie No.1 128 Kbps.mp3",
+      
+    }
+  ];
